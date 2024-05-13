8ed7ae7034be6f0e993af0e66de171dc3a020192
Marion <Marion.Hethuin@uphf.fr>
2024-05-13 12:08:59 +0200

    push test Robot

---
+++ src/main/java/org/uphf/projetsae201/Robot.java
@@ -0,0 +1,43 @@
+package org.uphf.projetsae201;
+
+import jdk.jfr.Event;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Robot {
+    private int capciteExtraction;
+    private int capaciteStockage;
+    private int nbMineraisExtraits;
+    private ArrayList<String> direction;
+
+    public Robot(){
+        this.nbMineraisExtraits= new Random().nextInt(10-5) + 5;
+
+
+    }
+
+    public void deplacer(Event e){
+
+    }
+
+    public void extraire(){
+
+    }
+
+    public void seVider(){
+
+    }
+
+    public int getCapaciteExtraction(){
+        return 0;
+    }
+
+    public int getCapaciteStockage(){
+        return 0;
+    }
+
+    public ArrayList<String> getDirection(){
+
+    }
+}
