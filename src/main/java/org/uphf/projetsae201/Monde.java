89303f09bf95542ef609570788e22d4de94ff299
Remy <remy.boulet@uphf.fr>
2024-05-13 12:29:13 +0200

    Monde création des attributs

--- src/main/java/org/uphf/projetsae201/Monde.java
+++ src/main/java/org/uphf/projetsae201/Monde.java
@@ -1,4 +1,21 @@
 package org.uphf.projetsae201;
 
 public class Monde {
+    int nbTerrains;
+    int nbMines;
+    int nbEntrepots;
+    int compteurTour;
+    int nbRobot;
+    int nbPlantEau;
+    int longueurMonde;
+    int largeurMonde;
+
+    public int getCompteurTour() {
+        return this.compteurTour;
+    }
+
+    public Monde(){
+
+
+    }
 }
