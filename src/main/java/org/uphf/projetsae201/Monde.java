764ac1fae15f12b72582f368dd430b27b25cdca6
Remy <remy.boulet@uphf.fr>
2024-05-17 11:12:33 +0200

    création Monde constructeur

--- src/main/java/org/uphf/projetsae201/Monde.java
+++ src/main/java/org/uphf/projetsae201/Monde.java
@@ -30,14 +30,19 @@ public class Monde {
         this.longueurMonde = longueurMonde;
         this.largeurMonde = largeurMonde;
 
-        // le nb de terrains represente au moins 90% +- 10%
-        this.nbTerrains = (int) (0.9*(this.largeurMonde*this.longueurMonde) + new Random(1).nextInt((int) (this.largeurMonde*this.longueurMonde*0.1))-1);
+        // le nb de terrains represente au moins 90% +- 10% -
+        this.nbTerrains = (int) (0.9*(this.largeurMonde*this.longueurMonde))+new Random().nextInt((int) (0.1*(this.largeurMonde*this.longueurMonde)+1))-1;
+
+        //System.out.println((int) (0.9*(this.largeurMonde*this.longueurMonde)+0.1*(0))-1);
+        //System.out.println((int) (0.9*(this.largeurMonde*this.longueurMonde)+0.1*(this.largeurMonde*this.longueurMonde)-1));
+        //System.out.println(this.nbTerrains);
+
 
         // Mme Lepreux nous a dit au moins un plans d'eau.
         this.nbPlanEau = 1 + this.largeurMonde*this.longueurMonde+this.nbTerrains;
 
         // au moins une mine d'or et une mine de nickel
-        this.nbMines = 2+ new Random(0).nextInt(2);
+        this.nbMines = 2+ new Random().nextInt(2);
 
         //on crée un tableau car on connait le nombre de case et c'est fixe
         this.lstSecteur = new Secteur[this.longueurMonde][this.largeurMonde];
@@ -83,14 +88,14 @@ public class Monde {
         ArrayList<Integer> temp = new ArrayList<Integer>();
         ArrayList<ArrayList<Integer>> pas = new ArrayList<ArrayList<Integer>>(5) ; //secteur deja choisi
 
-
+        System.out.println(this.longueurMonde);
 
 
         for (int x = 0; x < this.nbPlanEau;) {
 
-            l = new Random(0).nextInt(this.longueurMonde);
-            L = new Random(0).nextInt(this.largeurMonde);
-            System.out.println(l);
+            l = new Random().nextInt(this.longueurMonde);
+            L = new Random().nextInt(this.largeurMonde);
+
             temp.add(l);
             temp.add(L);
 
@@ -98,8 +103,9 @@ public class Monde {
 
                 pas.add(temp);
                 System.out.println(pas);
-                this.lstSecteur[temp]  = new Terrain();
+                this.lstSecteur[temp.get(0)][temp.get(1)]  = new Terrain();
                 x+=1;
+                System.out.println(this.lstSecteur[temp.get(0)][temp.get(1)]);
 
 
             }
