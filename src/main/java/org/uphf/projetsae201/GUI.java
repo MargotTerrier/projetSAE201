5c8f4603ec4cf337a461fe5dad5393a429730d56
Marion <Marion.Hethuin@uphf.fr>
2024-05-21 16:36:09 +0200

    push !

--- src/main/java/org/uphf/projetsae201/GUI.java
+++ src/main/java/org/uphf/projetsae201/GUI.java
@@ -1,5 +1,13 @@
 package org.uphf.projetsae201;
 
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class GUI extends Stage{
@@ -7,8 +15,32 @@ public class GUI extends Stage{
     public GUI(Monde m ){
 
     }
-    public GUI(Stage stage){
 
+    public GUI(){
+        Group g = new Group();
+        Scene scene = new Scene(g, 1440, 811);
+
+        Button quit = new Button("Quitter le jeu");
+        quit.setLayoutX(10);
+        quit.setLayoutY(10);
+        quit.setFont(new Font("Arial", 15));
+
+        Button redemarrer = new Button("Redémarrer une partie");
+        redemarrer.setLayoutX(140);
+        redemarrer.setLayoutY(10);
+        redemarrer.setFont(new Font("Arial", 15));
+
+        Label tour = new Label("Tour ");
+        tour.setLayoutY(10);
+        tour.layoutXProperty().bind(scene.widthProperty().subtract(tour.prefWidth(-1)).divide(2.1));
+        tour.setFont(new Font("Arial", 30));
+
+
+        g.getChildren().addAll(quit, redemarrer, tour);
+        this.getIcons().add(new Image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlXw-Np3cMZ4kj518EfxS3uKmiZ6Wx6tbHvJ2CJRBVxA&s"));
+        this.setTitle("Jeu de la mine");
+        this.setScene(scene);
+        this.show();
 
     }
 }
