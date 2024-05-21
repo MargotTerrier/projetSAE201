ba21463b621d07b5a0fd504e999286e516821d8e
Marion <Marion.Hethuin@uphf.fr>
2024-05-21 16:38:53 +0200

    push !

--- src/main/java/org/uphf/projetsae201/Main.java
+++ src/main/java/org/uphf/projetsae201/Main.java
@@ -1,12 +1,6 @@
 package org.uphf.projetsae201;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -16,14 +10,7 @@ public class Main extends Application {
         System.out.println(m);
         new AffichageConsole(m.getLstSecteur());
 
-
-        Text text = new Text(10,40,"PAS D'ERREUR");
-        text.setFont(new Font("Arial",20));
-        Scene scene = new Scene(new Group(text));
-        stage.getIcons().add(new Image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlXw-Np3cMZ4kj518EfxS3uKmiZ6Wx6tbHvJ2CJRBVxA&s"));
-        stage.setTitle("Jeu de la mine");
-        stage.setScene(scene);
-        stage.show();
+        new GUI();
     }
     public static void main(String[] args) {
         Application.launch(args);
