79bb64e29086d0c71c1b84adcf9ab6b33bae5113
arthur <Arthur.Machabeis-Berthome@uphf.fr>
2024-05-21 15:32:46 +0200

    Définition de la méthode extraction

--- src/main/java/org/uphf/projetsae201/Mine.java
+++ src/main/java/org/uphf/projetsae201/Mine.java
@@ -4,16 +4,14 @@ import java.util.Random;
 
 public class Mine extends District{
     private int id;
-    private int nbMinerais;
+    private int nbMinerais; // nombre de minerais actuel dans la mine
     private Minerai typeMinerai;
-    private int capacite;
-    private boolean vide;
+    private int capacite; // nombre de minerais total présent dans la mine au départ
 
     public Mine(int id){
         this.id = id;
         this.nbMinerais = 50 + new Random().nextInt(50);
         this.capacite = nbMinerais;
-        this.vide = false;
     }
 
     public int getId(){
@@ -26,16 +24,17 @@ public class Mine extends District{
         return this.capacite;
     }
 
-    public void extraction(Robot r){
-        this.nbMinerais -= r.getCapaciteExtraction();
-    }
-
-    public void setVide(boolean v) {
-        this.vide=v;
-    }
-
-    public boolean getVide(){
-        return this.vide;
+    public int extraction(Robot r){
+        if (this.nbMinerais == 0) return -1;
+        else if (this.nbMinerais - r.getCapaciteExtraction() < 0) {
+            int tmp = this.nbMinerais;
+            this.nbMinerais = 0;
+            return tmp;
+        }
+        else {
+            this.nbMinerais -= r.getCapaciteExtraction();
+            return r.getCapaciteExtraction();
+        }
     }
 
 }
