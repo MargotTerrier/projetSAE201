28b2723bb5f6b12c7bdaf7e574697dc3b06163f0
Remy <remy.boulet@uphf.fr>
2024-05-22 09:21:23 +0200

    affichage en console

--- src/main/java/org/uphf/projetsae201/View/AffichageConsole.java
+++ src/main/java/org/uphf/projetsae201/View/AffichageConsole.java
@@ -1,14 +1,12 @@
 package org.uphf.projetsae201.View;
 
-import org.uphf.projetsae201.Model.Entrepot;
-import org.uphf.projetsae201.Model.Monde;
-import org.uphf.projetsae201.Model.Secteur;
-import org.uphf.projetsae201.Model.Terrain;
+import org.uphf.projetsae201.Model.*;
 
 public class AffichageConsole {
     private Monde m ;
     public AffichageConsole(Monde m ) {
         this.m = m;
+        affichage();
     }
 
     public void affichage() {
@@ -17,27 +15,48 @@ public class AffichageConsole {
 
         //*2 car on a 4 case par secteur
         for(int i = 0; i < map.length*2; i++) {
-            String s =new String(new char[map[i].length*2]).replace("\0", "=");
-            System.out.print(s);
+            String s =new String(new char[map.length*2]).replace("\0", "=");
+            System.out.println(s);
+            temp = "|";
 
             for(int j = 0; j < map[i].length; j++) {
 
                 //On sépare les cas en paire et impair afin de savoir si on est sur la ligne des mine ou des Robots.
                 if (i%2 == 0) {
-                    else if ((map[i][j]instanceof Terrain){
+                    if ((map[i][j]instanceof Terrain)) {
                         Terrain t = (Terrain) map[i][j];
-                        if (t.getDistrict()==null){
-
+                        if (t.getDistrict() instanceof Entrepot) {
+                            temp += "E " + ((Entrepot) t.getDistrict()).getId() + "|";
                         }
-                        else if (t.getDistrict() instanceof Entrepot){
-
+                        else if (t.getDistrict() instanceof Mine) {
+                            temp += "M " + ((Mine) t.getDistrict()).getId() + "|";
                         }
-
+                        else {
+                            temp += "   |";
+                        }
                     }
-
+                    else if ((map[i][j]instanceof PlanDeau)){
+                        temp += "X X|";
+                    }
+                }
+                else {
+                    if (map[i][j] instanceof Terrain) {
+                        Terrain t = (Terrain) map[i][j];
+                        if(t.getRobot() != null) {
+                            temp += "R " + t.getRobot().getIdRobot() + "|";
+                        }
+                        else  {
+                            temp += "   |";
+                        }
+                    }
+                    else if ((map[i][j]instanceof PlanDeau)){
+                        temp += "X X|";
+                    }
                 }
             }
+            System.out.println(temp);
         }
+
     }
 
 
