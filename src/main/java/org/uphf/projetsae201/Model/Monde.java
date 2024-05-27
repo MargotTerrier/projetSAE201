5a641e8e3ab0ebb14204947e3933860d6f955713
Margot <margot.terrier@uphf.fr>
2024-05-27 14:14:33 +0200

    création classes de test (bonne façon cette fois) et ajout constructeur dans monde

--- src/main/java/org/uphf/projetsae201/Model/Monde.java
+++ src/main/java/org/uphf/projetsae201/Model/Monde.java
@@ -19,6 +19,10 @@ public class Monde {
 
     private Secteur[][] lstSecteur;
 
+    public Monde(int longueurMonde, int largeurMonde) {
+        this.longueurMonde = longueurMonde;
+        this.largeurMonde = largeurMonde;
+    }
 
     public Monde(int nbEntrepots, int nbRobot, int longueurMonde, int largeurMonde) {
 
@@ -262,7 +266,6 @@ public class Monde {
 
             }
         }
-
     }
 
     public void extraire(Robot robot, Mine mine){
