c801c57a9eb16c61129886deba6dfeef78f0ef90
Remy <remy.boulet@uphf.fr>
2024-05-27 19:15:45 +0200

    event

--- src/main/java/org/uphf/projetsae201/Controller/VGraphique.java
+++ src/main/java/org/uphf/projetsae201/Controller/VGraphique.java
@@ -9,12 +9,12 @@ public class VGraphique {
     private GUI gui;
     private Monde m;
     private EventGUI eg;
-    private String[] mouvement;
+    private int cpt;
+
 
     public VGraphique() {
         this.m = new Monde(2, new Random().nextInt(2, 5), 10, 10);
         gui = new GUI(m);
-        this.mouvement = new String[this.m.getNbRobot()];
         this.eg = new EventGUI(this);
 
 
@@ -27,31 +27,16 @@ public class VGraphique {
         return this.m;
     }
 
-    public void actualise(int i , String mouve){
-        boolean depl = true;
-        this.mouvement[i] = mouve;
-        System.out.println(this.mouvement[i]);
-        for (String verif : this.mouvement){
-            if (verif==null){
-                depl=false;
-            }
-        }
-
-        if (depl && mouve == "Valider"){
-            int index=0;
-            for (Robot d : this.m.getRobots()){
+    public void actualise(){
 
 
-                index += 1;
-            }
-
+        if(getMonde().verifFin()){
+            System.out.println("fin");
+            gui.close();
         }
         else {
 
-            gui.setMonde(m);
-            gui.Affiche(eg);
         }
-
 
 
     }
